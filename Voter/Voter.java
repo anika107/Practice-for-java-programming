c348caecf4b11a02b9ec9d66eefdfa77242767a8
Anika <32257753+anika107@users.noreply.github.com>
2018-07-24 01:38:56 +0600

    Add files via upload

---
+++ Voter/Voter.java
@@ -0,0 +1,15 @@
+
+public class Voter {
+    public String name;
+    public String id;
+    public int age;
+
+    public Voter(String name, String id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+    
+    
+    
+}
