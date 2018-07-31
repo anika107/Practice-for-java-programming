cf70b95f363af422d310b91acb788273a2c8ac0c
Anika <32257753+anika107@users.noreply.github.com>
2018-07-31 19:20:32 +0600

    Update ClubMember.java

--- Person/ClubMember.java
+++ Person/ClubMember.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.oop.bambooA;
 
 import com.oop.bambooB.Student;
 
-/**
- *
- * @author DOLPHIN
- */
+
 final class ClubMember extends Student{
     public String status;
 
