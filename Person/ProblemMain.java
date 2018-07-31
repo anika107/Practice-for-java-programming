d6b243d1a4cb290018ab8b138aaf4a96238a8670
Anika <32257753+anika107@users.noreply.github.com>
2018-07-31 19:21:51 +0600

    Update ProblemMain.java

--- Person/ProblemMain.java
+++ Person/ProblemMain.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.oop.bambooA;
 
 import com.oop.bambooB.Student;
 import com.oop.bambooC.Person;
 
-/**
- *
- * @author DOLPHIN
- */
 public class ProblemMain {
     public static void main(String[] args) {
     Student student= new Student("011151");
