7ad0da02de2b9e6387724bfa99cf106627f7812e
Anika <32257753+anika107@users.noreply.github.com>
2018-07-31 16:20:30 +0600

    Update ClassRoom.java

--- Room/ClassRoom.java
+++ Room/ClassRoom.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author DOLPHIN
- */
 public class ClassRoom extends Room{
     private String department;
     private boolean isProjectAvailable;
