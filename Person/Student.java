ddbf0c306ff215cac0b1901968725c80b8a39b5d
Anika <32257753+anika107@users.noreply.github.com>
2018-07-31 19:22:21 +0600

    Update Student.java

--- Person/Student.java
+++ Person/Student.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.oop.bambooB;
 import com.oop.bambooC.Person;
 
-/**
- *
- * @author DOLPHIN
- */
+
 public class Student extends Person{
     public String id;
      public Student(String id) {
